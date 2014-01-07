79772cd79f1798b8f118eead18720274d4bbd9fb
Attila Oláh <attilaolah@gmail.com>
2014-01-07 19:38:05 +0700

    fix unmarshal of categories

--- types/categories.go
+++ types/categories.go
@@ -7,17 +7,18 @@ import (
 
 type Categories map[int64]string
 
-func (c Categories) UnmarshalJSON(b []byte) (err error) {
+func (c *Categories) UnmarshalJSON(b []byte) (err error) {
 	var m map[string]string
 	if err = json.Unmarshal(b, &m); err != nil {
 		return
 	}
 	var i int64
+	*c = make(Categories, len(m))
 	for k, v := range m {
 		if i, err = strconv.ParseInt(k, 10, 64); err != nil {
 			break
 		}
-		c[i] = v
+		(*c)[i] = v
 	}
 	return
 }
