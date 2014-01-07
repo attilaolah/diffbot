7c5241425c1185929cbf6700e202787a5759cc8d
Attila Oláh <attilaolah@gmail.com>
2014-01-07 20:13:25 +0700

    add datetime parsing test

---
+++ types/time_test.go
@@ -0,0 +1,32 @@
+package types
+
+import (
+	"encoding/json"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestTime(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Time Suite")
+}
+
+var _ = Describe("Time", func() {
+	Describe("JSON-deserialising", func() {
+		Context("An RFC1123-encoded timestamp", func() {
+			It("should return a valid Time object", func() {
+				var t Time
+				b := []byte(`"Tue, 7 Jan 2014 20:02:24 ICT"`)
+				err := json.Unmarshal(b, &t)
+				Expect(err).NotTo(HaveOccurred())
+				ict, err := time.LoadLocation("Asia/Bangkok")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(t.Time().Equal(time.Date(2014, time.January, 7, 20, 2, 24, 0, ict))).To(BeTrue())
+			})
+		})
+	})
+})
