1d436910646de80c4947f36e45b32f52a182f50b
Attila Oláh <attilaolah@gmail.com>
2014-01-07 19:38:20 +0700

    add test suite for unmarshalling categories

---
+++ types/categories_test.go
@@ -0,0 +1,39 @@
+package types
+
+import (
+	"encoding/json"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestCategories(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Categories Suite")
+}
+
+var _ = Describe("Categories", func() {
+	Describe("JSON-deserialising", func() {
+		Context("An object with string keys", func() {
+			It("should return a valid Categories object", func() {
+				var c Categories
+				b := []byte(`{
+				  "0": "zero",
+				  "1": "one",
+				  "2": "two",
+				  "-8": "minus eight"
+				}`)
+				err := json.Unmarshal(b, &c)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(c).To(Equal(Categories{
+					0: "zero",
+					1: "one",
+					2: "two",
+					-8: "minus eight",
+				}))
+			})
+		})
+	})
+})
