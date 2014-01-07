f3f125d105b6ae2c711ea53a5a7c11f8942d9ac8
Attila Oláh <attilaolah@gmail.com>
2014-01-07 20:13:17 +0700

    implement datetime parsing

--- types/time.go
+++ types/time.go
@@ -1,9 +1,14 @@
 package types
 
 import (
+	"encoding/json"
 	"time"
 )
 
+const (
+	// Modified RFC1123 format
+	mRFC1123 = "Mon, 2 Jan 2006 15:04:05 MST"
+)
 type Time time.Time
 
 func (t Time) Time() time.Time {
@@ -11,6 +16,17 @@ func (t Time) Time() time.Time {
 }
 
 func (t *Time) UnmarshalJSON(b []byte) (err error) {
-	// TODO: unmarshal! (try the original, the nfall back to a custom format)
+	var tt time.Time
+	if err = json.Unmarshal(b, &tt); err == nil {
+		*t = Time(tt)
+		return // correct encoding
+	}
+	// Try decoding as a string
+	var s string
+	if err = json.Unmarshal(b, &s); err != nil {
+		return
+	}
+	tt, err = time.Parse(mRFC1123, s)
+	*t = Time(tt)
 	return
 }
